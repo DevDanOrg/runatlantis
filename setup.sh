9a0b4cc0d8150c6415c712ca99d87a0b7e6695e4
nwokolo <55523993+nwokolo24@users.noreply.github.com>
2025-01-08 23:12:19 -0800

    update config

--- setup.sh
+++ setup.sh
@@ -16,26 +16,26 @@ if [ -f atlantis ]; then
     --gh-webhook-secret="$SECRET" \
     --repo-allowlist="$REPO_ALLOWLIST" \
     --repo-config="$REPO_CONFIG"
+else 
+    ATLANTIS_VERSION=0.32.0
+
+    wget https://github.com/runatlantis/atlantis/archive/refs/tags/v${ATLANTIS_VERSION}.tar.gz
+    tar -xzf v${ATLANTIS_VERSION}.tar.gz
+
+    cd atlantis-${ATLANTIS_VERSION}
+    go build -o ../atlantis
+    cd ..
+
+    rm -rf atlantis-${ATLANTIS_VERSION}
+    rm v${ATLANTIS_VERSION}.tar.gz*
+
+    . ./atlantis.env
+
+    ./atlantis server \
+    --atlantis-url="$URL" \
+    --gh-user="$USERNAME" \
+    --gh-token="$TOKEN" \
+    --gh-webhook-secret="$SECRET" \
+    --repo-allowlist="$REPO_ALLOWLIST"
+    --repo-config="$REPO_CONFIG"
 fi
-
-ATLANTIS_VERSION=0.32.0
-
-wget https://github.com/runatlantis/atlantis/archive/refs/tags/v${ATLANTIS_VERSION}.tar.gz
-tar -xzf v${ATLANTIS_VERSION}.tar.gz
-
-cd atlantis-${ATLANTIS_VERSION}
-go build -o ../atlantis
-cd ..
-
-rm -rf atlantis-${ATLANTIS_VERSION}
-rm v${ATLANTIS_VERSION}.tar.gz*
-
-. ./atlantis.env
-
-./atlantis server \
---atlantis-url="$URL" \
---gh-user="$USERNAME" \
---gh-token="$TOKEN" \
---gh-webhook-secret="$SECRET" \
---repo-allowlist="$REPO_ALLOWLIST"
---repo-config="$REPO_CONFIG"
