6c655ded01e24c34b020f37a4a42c4781677a808
nwokolo <55523993+nwokolo24@users.noreply.github.com>
2025-01-08 21:58:07 -0800

    update config

--- setup.sh
+++ setup.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+# check if atlantis binary exist before downloading tar.gz
+if [ -f atlantis ]; then
+    echo "atlantis binary already exists running atlantis..."
+
+    # env vars
+    . ./atlantis.env
+    
+    ./atlantis server \
+    --atlantis-url="$URL" \
+    --gh-user="$USERNAME" \
+    --gh-token="$TOKEN" \
+    --gh-webhook-secret="$SECRET" \
+    --repo-allowlist="$REPO_ALLOWLIST"
+    --repo-config="$REPO_CONFIG"
+fi
+
 ATLANTIS_VERSION=0.32.0
 
 wget https://github.com/runatlantis/atlantis/archive/refs/tags/v${ATLANTIS_VERSION}.tar.gz
